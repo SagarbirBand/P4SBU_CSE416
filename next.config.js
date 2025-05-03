9abdcdfe4af07458a35225f0c1aa48b065bd19bc
SagarbirBand <s.bandesha2021@gmail.com>
2025-05-03 19:50:06 -0400

    netTest3

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 
-/*const withPlugins = require("next-compose-plugins");
-const withNextOnNetlify = require("next-on-netlify");
+//const withPlugins = require("next-compose-plugins");
+//const withNextOnNetlify = require("next-on-netlify");
 
 const nextConfig = {
   experimental: {
-    appDir: true, // Enables the new app directory (Next.js 13+)
-  },
-};*/
-
-module.exports = {}
+    appDir: true,
+  }
+}
